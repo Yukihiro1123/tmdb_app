09489559069713e5f5945939e2c3479c6bbdcd50
Yukihiro1123 <murasima1123@gmail.com>
2023-09-10 10:47:30 +0900

    映画カードコンポーネント作成

---
+++ lib/src/common_widgets/cached_image.dart
@@ -0,0 +1,53 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class CachedImage extends StatelessWidget {
+  final String? imageURL;
+  final double width;
+  final double height;
+  final bool isCircle;
+  const CachedImage({
+    super.key,
+    required this.imageURL,
+    required this.width,
+    required this.height,
+    required this.isCircle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageURL ??
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fmovie&psig=AOvVaw0bx0ln6dk7Zx_aNI95U_C8&ust=1694396662475000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCOic4sr1noEDFQAAAAAdAAAAABAE',
+      imageBuilder: (context, imageProvider) => Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
+          image: DecorationImage(
+            fit: BoxFit.contain,
+            image: imageProvider,
+          ),
+        ),
+      ),
+      placeholder: (context, url) => Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
+        ),
+      ),
+      errorWidget: (context, url, dynamic error) {
+        print(error);
+        return Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
+            color: Colors.red,
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/src/features/movies/views/component/movie_card.dart
@@ -0,0 +1,63 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tmdb_app/src/common_widgets/cached_image.dart';
+import 'package:tmdb_app/src/features/movies/data_model/movie_response/movie/movie.dart';
+import 'package:tmdb_app/src/routing/router_utils.dart';
+
+class MovieCard extends StatelessWidget {
+  final Movie item;
+  final VoidCallback onTap;
+
+  const MovieCard({
+    super.key,
+    required this.item,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => context.goNamed(AppRoute.movie.name, queryParameters: {
+        "movieId": item.id.toString(),
+      }),
+      child: Card(
+        elevation: 12,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        // color: Colors.black,
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(20),
+                topLeft: Radius.circular(20),
+              ),
+              child: CachedImage(
+                imageURL: item.backdropPath,
+                width: double.infinity,
+                height: 200,
+                isCircle: false,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                item.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: Text(
+                item.voteAverage.toString(),
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
