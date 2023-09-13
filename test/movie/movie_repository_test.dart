ec33566620edd5a4986aac79b423c1bf06fcde17
Yukihiro1123 <murasima1123@gmail.com>
2023-09-13 22:49:42 +0900

    dioをmockにした

--- test/movie/movie_repository_test.dart
+++ test/movie/movie_repository_test.dart
@@ -2,37 +2,48 @@ import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:tmdb_app/env.dart';
 import 'package:tmdb_app/src/features/movies/data_model/movie_response/movie_response.dart';
 import 'package:tmdb_app/src/features/movies/repository/movie_repository.dart';
 import 'package:tmdb_app/src/utils/dio/dio_provider.dart';
 import 'package:http/http.dart' as http;
-import 'movie_repository_test.mocks.dart';
 
-class MockMovieRepository extends AutoDisposeNotifier<Dio>
-    with Mock
-    implements MovieRepository {}
+class MockDio extends AutoDisposeNotifier<Dio> with Mock implements Dio {}
 
 void main() {
-  late MockMovieRepository _repository;
   late MockDio _dio;
   late ProviderContainer container;
 
   setUp(() {
-    _repository = MockMovieRepository();
     _dio = MockDio();
     container = ProviderContainer(
       overrides: [
         dioProvider.overrideWith((ref) => _dio),
-        movieRepositoryProvider.overrideWith(() => _repository),
       ],
     );
   });
 
   group('getNowPlayingMovies', () {
+    final url = Uri(
+      scheme: 'https',
+      host: 'api.themoviedb.org',
+      path: '3/movie/now_playing',
+      queryParameters: {
+        'language': 'ja-JP',
+        'with_original_language': 'ja',
+        'api_key': Env.apiKey,
+        'include_adult': 'false',
+        'page': '1',
+      },
+    ).toString();
     test('MovieResponse型のデータを返す.', () async {
-      when(() => _repository.getNowPlayingMovies(page: 1))
-          .thenAnswer((_) async => MovieResponse.fromJson(mockResponse));
-      // print(movieResponse);
+      when(() => _dio.get(url)).thenAnswer(
+        (_) async => Response(
+          statusCode: 200,
+          data: mockResponse,
+          requestOptions: RequestOptions(baseUrl: url),
+        ),
+      );
       final movieResponse = await container
           .read(movieRepositoryProvider.notifier)
           .getNowPlayingMovies(page: 1);
@@ -40,8 +51,7 @@ void main() {
     });
 
     test('API呼び出しの際にエラーが出たら例外を返す', () async {
-      when(() => _repository.getNowPlayingMovies(page: 1))
-          .thenThrow(Exception('Failed to load movie'));
+      when(() => _dio.get(url)).thenThrow(Exception('Failed to load movie'));
 
       expect(
           () => container
