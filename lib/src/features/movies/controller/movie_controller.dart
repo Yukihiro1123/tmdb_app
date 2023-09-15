69dd51e59140468044a9a31dee8cbc58bfde818e
Yukihiro1123 <murasima1123@gmail.com>
2023-09-15 16:59:33 +0900

    update: 映画検索ウィジェットをstatelessにする

--- lib/src/features/movies/controller/movie_controller.dart
+++ lib/src/features/movies/controller/movie_controller.dart
@@ -53,16 +53,19 @@ class MovieController extends _$MovieController {
     }
   }
 
-  Future<MovieResponse> searchMovie({
+  Future<void> searchMovie({
     required String query,
     required int page,
+    required void Function(MovieResponse) onSuccess,
+    required void Function(String) onError,
   }) async {
     try {
-      return await ref
+      final response = await ref
           .read(movieRepositoryProvider.notifier)
           .searchMovie(query: query, page: page);
+      onSuccess(response);
     } catch (e) {
-      rethrow;
+      onError('error');
     }
   }
 }
--- lib/src/features/movies/views/search_movie_page.dart
+++ lib/src/features/movies/views/search_movie_page.dart
@@ -1,69 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:tmdb_app/src/features/movies/controller/movie_controller.dart';
 import 'package:tmdb_app/src/features/movies/data_model/movie_response/movie/movie.dart';
+import 'package:tmdb_app/src/features/movies/views/component/movie_card.dart';
 import 'package:tmdb_app/src/features/movies/views/component/movie_list.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:tmdb_app/src/utils/breakpoints.dart';
 
-class SearchMoviePage extends StatefulHookConsumerWidget {
+class SearchMoviePage extends HookConsumerWidget {
   const SearchMoviePage({super.key});
-
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _SearchMoviePageState();
-}
-
-class _SearchMoviePageState extends ConsumerState<SearchMoviePage> {
-  final PagingController<int, Movie> _pagingController =
-      PagingController(firstPageKey: 1);
-
-  String? _searchTerm;
-  @override
-  void initState() {
-    _pagingController.addPageRequestListener((pageKey) {
-      _fetchPage(pageKey);
-    });
-    super.initState();
-  }
-
-  Future<void> _fetchPage(int pageKey) async {
-    try {
-      final newItems = await ref
-          .read(movieControllerProvider.notifier)
-          .searchMovie(page: pageKey, query: _searchTerm ?? '');
-      if (mounted) {
-        final isLastPage = newItems.page == newItems.totalPages;
-
-        if (isLastPage) {
-          _pagingController.appendLastPage(newItems.results);
-        } else {
-          final nextPageKey = pageKey + 1;
-          _pagingController.appendPage(newItems.results, nextPageKey);
-        }
-      }
-    } catch (error) {
-      print(error);
-      _pagingController.error = error;
-    }
-  }
-
-  void _updateSearchTerm(String searchTerm) {
-    _searchTerm = searchTerm;
-    _pagingController.refresh();
-  }
-
-  @override
-  void dispose() {
-    _pagingController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final PagingController<int, Movie> _pagingController =
+        PagingController(firstPageKey: 1);
+    final isMounted = useIsMounted();
+    final usersViewModel = ref.watch(
+      movieControllerProvider.notifier,
+    );
+    final double screenWidth = MediaQuery.sizeOf(context).width;
     final TextEditingController searchController = useTextEditingController();
     final hasSearched = useState(false);
+
+    useEffect(
+      () {
+        _pagingController.addPageRequestListener((pageKey) {
+          usersViewModel.searchMovie(
+              query: searchController.text,
+              page: pageKey,
+              onSuccess: (data) {
+                if (isMounted()) {
+                  if (data.page == data.totalPages) {
+                    _pagingController.appendLastPage(data.results);
+                  } else {
+                    _pagingController.appendPage(data.results, pageKey + 1);
+                  }
+                }
+              },
+              onError: (error) {
+                // print(error);
+                _pagingController.error = error;
+              });
+        });
+        return () => _pagingController.dispose();
+      },
+      const [],
+    );
+
     return SafeArea(
       child: Scaffold(
         body: Column(
@@ -75,29 +60,61 @@ class _SearchMoviePageState extends ConsumerState<SearchMoviePage> {
                 IconButton(
                   icon: const Icon(Icons.search),
                   onPressed: () {
-                    hasSearched.value = true;
-                    _updateSearchTerm(searchController.text);
+                    _pagingController.refresh();
                   },
                 ),
               ],
             ),
             const SizedBox(height: 10),
             Expanded(
-              child: MovieList(
-                pagingController: _pagingController,
-                noItemsFoundWidget: Center(
-                  heightFactor: 6,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(Icons.search, size: 50),
-                      Text(
-                        hasSearched.value == false
+              child: RefreshIndicator(
+                onRefresh: () => Future.sync(
+                  () => _pagingController.refresh(),
+                ),
+                child: PagedGridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: screenWidth <= BreakPoints.mobileSize
+                        ? 1.4
+                        : screenWidth <= BreakPoints.tabletSize
+                            ? 1.25
+                            : 1.4,
+                    crossAxisSpacing: 5,
+                    //TODO ここどうするか考える
+                    crossAxisCount: screenWidth <= BreakPoints.mobileSize
+                        ? 1
+                        : screenWidth <= BreakPoints.tabletSize
+                            ? 2
+                            : 4,
+                  ),
+                  pagingController: _pagingController,
+                  builderDelegate: PagedChildBuilderDelegate<Movie>(
+                    noItemsFoundIndicatorBuilder: (context) {
+                      return Center(
+                          child: Text(
+                        searchController.text.isEmpty
                             ? AppLocalizations.of(context).searchByKeyword
                             : AppLocalizations.of(context).movieNotFound,
                         style: Theme.of(context).textTheme.bodyLarge,
-                      )
-                    ],
+                      ));
+                    },
+                    itemBuilder: (context, item, index) {
+                      return AnimationConfiguration.staggeredGrid(
+                        position: index,
+                        columnCount: screenWidth <= BreakPoints.mobileSize
+                            ? 1
+                            : screenWidth <= BreakPoints.tabletSize
+                                ? 2
+                                : 4,
+                        child: SlideAnimation(
+                          child: FadeInAnimation(
+                            child: MovieCard(
+                              item: item,
+                              onTap: () {},
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
