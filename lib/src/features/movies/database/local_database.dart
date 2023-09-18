65494ffa20c02914dc9d27a55174eb64e9cb1547
Yukihiro1123 <murasima1123@gmail.com>
2023-09-18 15:22:28 +0900

    refactor: :fire: 不要ファイル削除

--- lib/src/features/movies/database/local_database.dart
+++
@@ -1,9 +0,0 @@
-import 'database.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-part "local_database.g.dart";
-
-@riverpod
-LocalDatabase localDatabase(LocalDatabaseRef ref) {
-  final localDatabase = LocalDatabase();
-  return localDatabase;
-}
--- lib/src/features/movies/repository/movie_repository.dart
+++ lib/src/features/movies/repository/movie_repository.dart
@@ -6,7 +6,6 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:tmdb_app/src/features/movies/data_model/movie_response/movie/movie.dart';
 import 'package:tmdb_app/src/features/movies/data_model/movie_response/movie_response.dart';
 import 'package:tmdb_app/src/features/movies/data_model/review_response/review_response.dart';
-import 'package:tmdb_app/src/features/movies/database/local_database.dart';
 
 import 'package:tmdb_app/src/utils/dio/dio_provider.dart';
 part 'movie_repository.g.dart';
