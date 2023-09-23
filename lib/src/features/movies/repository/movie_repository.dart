c23f2ab15c3a85b40735a7f4745b2d5301f489d5
Yukihiro1123 <murasima1123@gmail.com>
2023-09-24 00:09:21 +0900

    style: :bulb: sembastのtransactionについてコメント追加

--- lib/src/features/movies/repository/movie_repository.dart
+++ lib/src/features/movies/repository/movie_repository.dart
@@ -23,6 +23,15 @@ class MovieRepository extends _$MovieRepository {
     required String storePath,
     required Map<String, dynamic> response,
   }) async {
+    // 以下のコードだとunit testでmovieRecord.get(txn) as Map<String, dynamic>;がnullになる
+    // 実際は問題なさそうだが
+    // final store = intMapStoreFactory.store(storePath);
+    // var movieRecord = state.record(storePath);
+    // final result = await ref.read(databaseProvider).transaction((txn) async {
+    //   await store.delete(txn);
+    //   await store.add(txn, response);
+    //   return await movieRecord.get(txn) as Map<String, dynamic>;
+    // });
     //delete
     await state.record(storePath).delete(ref.read(databaseProvider));
     //insert
